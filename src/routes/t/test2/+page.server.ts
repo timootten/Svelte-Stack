ddb280ca703e59b4bd3972a7abd8991f2c6cb4d7
Timo <67387740+timootten@users.noreply.github.com>
2024-08-09 17:12:46 +0900

    +page.server.ts aktualisieren

--- src/routes/t/test2/+page.server.ts
+++ src/routes/t/test2/+page.server.ts
@@ -2,9 +2,10 @@ import { db } from "$lib/server/db";
 import { userTable } from "$lib/server/db/schema";
 import { sleep } from "$lib/utils";
 
-export async function load({ locals }) {
+export async function load({ locals, request }) {
   const users = await db.select().from(userTable);
-
+  console.log(request.headers);
+  console.log(Math.random());
 
   const takesLong = async () => {
     await sleep(5000)
