594d58bc9404c2eb01b84591084907fede84f671
Timo <67387740+timootten@users.noreply.github.com>
2024-08-11 01:27:45 +0900

    +layout.server.ts aktualisieren

--- src/routes/t/+layout.server.ts
+++ src/routes/t/+layout.server.ts
@@ -1,13 +1,12 @@
 
 
 export const load = async ({ request, locals, cookies }) => {
-  const secFetchSite = request.headers.get("sec-fetch-site");
-  console.log(secFetchSite);
-  let directHit = secFetchSite === "none" || secFetchSite === "cross-site";
-  console.log(directHit);
-  console.log(Math.random());
+  //const secFetchSite = request.headers.get("sec-fetch-site");
+  
+  //let directHit = secFetchSite === "none" || secFetchSite === "cross-site";
+ 
 
   return {
-     directHit,
+     //directHit,
   }
 };
