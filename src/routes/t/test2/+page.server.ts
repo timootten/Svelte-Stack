b15e650f93bd807426447b3a2a0dfe07d731c601
Timo <67387740+timootten@users.noreply.github.com>
2024-08-10 20:47:31 +0900

    +page.server.ts aktualisieren

--- src/routes/t/test2/+page.server.ts
+++ src/routes/t/test2/+page.server.ts
@@ -4,25 +4,14 @@ import { sleep } from "$lib/utils";
 import { redirect } from '@sveltejs/kit';
 
 export async function load({ locals, request, setHeaders }) {
-  const secFetchSite = request.headers.get("sec-fetch-site");
-  console.log(secFetchSite);
-  let directHit = secFetchSite === "none" || secFetchSite === "cross-site";
-  console.log(directHit);
-  console.log(Math.random());
-
-	setHeaders({
-		'sec-fetch-site': 'same-origin'
-	});
-
-  //if(directHit) redirect(302, '/t/test2');
-
+ 
   const takesLong = async () => {
     await sleep(5000)
     return { done: true }
   };
 
   return {
-    directHit: false,
+    
     x: {
        takesLong: takesLong()
     }
