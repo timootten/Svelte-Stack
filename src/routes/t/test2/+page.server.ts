5d523046f06c7788979c760e054e2299d4cfe7f6
Timo <67387740+timootten@users.noreply.github.com>
2024-08-10 19:30:33 +0900

    +page.server.ts aktualisieren

--- src/routes/t/test2/+page.server.ts
+++ src/routes/t/test2/+page.server.ts
@@ -1,6 +1,7 @@
 import { db } from "$lib/server/db";
 import { userTable } from "$lib/server/db/schema";
 import { sleep } from "$lib/utils";
+import { redirect } from '@sveltejs/kit';
 
 export async function load({ locals, request }) {
   const secFetchSite = request.headers.get("sec-fetch-site");
