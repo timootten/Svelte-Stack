dc879bf0f1b231214cfa541bd3b6bb4f5217b184
Timo <67387740+timootten@users.noreply.github.com>
2024-08-11 02:20:48 +0900

    +page.svelte aktualisieren

--- src/routes/t/test2/+page.svelte
+++ src/routes/t/test2/+page.svelte
@@ -8,13 +8,7 @@
 
     onMount(() => {
         if(data.directHit) {
-            alert("xxx");
-            //invalidate();
-						setTimeout(() => {
-              alert($page.url.pathname + $page.url.hash);
             invalidateAll();
-            }, 200);
-            //goto($page.url.pathname + $page.url.hash);
         }
     });
 </script>
