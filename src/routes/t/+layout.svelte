ac129e8ae060d71f4da289c331214f83a296e148
Timo <67387740+timootten@users.noreply.github.com>
2024-08-10 21:32:12 +0900

    +layout.svelte aktualisieren

--- src/routes/t/+layout.svelte
+++ src/routes/t/+layout.svelte
@@ -3,13 +3,14 @@
 	import Loading from '$lib/components/core/Loading.svelte';
     import { page } from '$app/stores';  
     import { onMount } from 'svelte';
-
+    import { invalidate } from '$app/navigation';
     export let data;
 
     onMount(() => {
         if(data.directHit) {
             alert("xxx");
-            goto($page.url.pathname + $page.url.hash);
+            invalidate($page.url.pathname + $page.url.hash);
+            //goto($page.url.pathname + $page.url.hash);
         }
     });
 </script>
