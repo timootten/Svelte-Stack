b3cf72c9b2320d63aa7fd6cc9b0a001fdf57800f
Timo <67387740+timootten@users.noreply.github.com>
2024-08-11 15:19:14 +0900

    +page.server.ts aktualisieren

--- src/routes/t/test2/+page.server.ts
+++ src/routes/t/test2/+page.server.ts
@@ -4,19 +4,19 @@ export async function load({ locals, request, setHeaders, depends }) {
   const secFetchSite = request.headers.get("sec-fetch-site");
   let directHit = secFetchSite === "none" || secFetchSite === "cross-site";
   
-  console.log("Called twice");
+  console.log("Called page");
 
   const takesShort = async () => {
     return { done: 'Xx' }
   };
 
-   if(directHit) return {
+   //if(directHit) return {
     /*directHit,
     clock: new Date().toLocaleString('de-de'),
     x: {
 			takesLong: takesShort()
     }*/
-  }
+  //}
 
   const takesLong = async () => {
     await sleep(5000)
