fe6fe915856fb2ebdac1eaacbc0e4c4e1500f0c6
Timo <67387740+timootten@users.noreply.github.com>
2024-08-12 03:19:33 +0900

    +layout.svelte aktualisieren

--- src/routes/t/+layout.svelte
+++ src/routes/t/+layout.svelte
@@ -11,8 +11,8 @@
 
     onMount(async () => {
         if(direct) {
-            await invalidateAll();
-            //await invalidate((url) => true);
+            //await invalidateAll();
+            await invalidate((url) => true);
 			      setTimeout(() => {
             	direct = false;
             }, 500);
