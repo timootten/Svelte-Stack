4e98dbe5274743624c413ee48affb8bec8a29bce
Timo <67387740+timootten@users.noreply.github.com>
2024-08-11 16:36:09 +0900

    +layout.server.ts aktualisieren

--- src/routes/t/+layout.server.ts
+++ src/routes/t/+layout.server.ts
@@ -2,9 +2,10 @@
 
 export const load = async ({ request, locals, cookies }) => {
   const secFetchSite = request.headers.get("sec-fetch-site");
-  let directHit = !secFetchSite || secFetchSite === "none" || secFetchSite === "cross-site";
- 
-  console.log("Layout called Twice");
+  const directHit = !secFetchSite || secFetchSite === "none" || secFetchSite === "cross-site";
+
+	console.log(secFetchSite, request);
+  console.log("Called layout", directHit);
 
   return {
      directHit,
