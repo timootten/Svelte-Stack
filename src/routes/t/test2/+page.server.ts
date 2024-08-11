4f02400f920dcc4c3e9b188386b9600f13e110a3
Timo <67387740+timootten@users.noreply.github.com>
2024-08-11 16:35:43 +0900

    +page.server.ts aktualisieren

--- src/routes/t/test2/+page.server.ts
+++ src/routes/t/test2/+page.server.ts
@@ -2,11 +2,9 @@ import { sleep } from "$lib/utils";
 
 export async function load({ locals, request, setHeaders, depends }) {
   const secFetchSite = request.headers.get("sec-fetch-site");
+  const directHit = !secFetchSite || secFetchSite === "none" || secFetchSite === "cross-site";
 
-  let directHit = !secFetchSite || secFetchSite === "none" || secFetchSite === "cross-site";
-  
 	console.log(secFetchSite, request);
-
   console.log("Called page", directHit);
 
   const takesShort = async () => {
