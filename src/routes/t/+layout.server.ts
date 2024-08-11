befa4a57b6187f597179272e1ebee13f1f701cde
Timo <67387740+timootten@users.noreply.github.com>
2024-08-11 19:06:29 +0900

    +layout.server.ts aktualisieren

--- src/routes/t/+layout.server.ts
+++ src/routes/t/+layout.server.ts
@@ -4,7 +4,7 @@ export const load = async ({ request, locals, cookies }) => {
   const secFetchSite = request.headers.get("sec-fetch-site");
   const directHit = !secFetchSite || secFetchSite === "none" || secFetchSite === "cross-site";
 
-	console.log(secFetchSite, request);
+	//console.log(secFetchSite, request);
   console.log("Called layout", directHit);
 
   return {
