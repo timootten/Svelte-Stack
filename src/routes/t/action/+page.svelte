76253d6b913be2c9babc80bc025f2254cd0d4eee
Timo Otten <67387740+timootten@users.noreply.github.com>
2024-08-15 18:54:16 +0200

    +page.svelte aktualisieren

--- src/routes/t/action/+page.svelte
+++ src/routes/t/action/+page.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
-	import { applyAction, enhance } from '$app/forms';
 	import type { actions } from './+page.server';
 	import { useAction } from './action.svelte';
- 	import { goto } from '$app/navigation';
- 	import { onMount } from 'svelte';
 
  	let { data, form } = $props();
  
