a0ca3c8bca2c81dd2e3a98a4c4e8525569da7263
Timo Otten <67387740+timootten@users.noreply.github.com>
2024-08-15 18:35:49 +0200

    action.svelte.ts aktualisieren

--- src/routes/t/action/action.svelte.ts
+++ src/routes/t/action/action.svelte.ts
@@ -1,15 +1,6 @@
 import { deserialize } from "$app/forms";
 import { onMount } from "svelte"
 
-interface UseActionOptions<T extends (...args: any) => Promise<any>> {
-  action: string;
-  interval: number;
-  defaultValue: Awaited<ReturnType<T>>;
-}
-
-// Todo: Remove action and use the function name
-// And make a copy and make a useAction
-
 export const useAction = <T extends (...args: any) => Promise<any>>(action: string) => {
   let value = $state<ReturnType<T>>(new Promise((resolve, reject) => { } ) as any)
 
